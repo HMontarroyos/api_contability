607ef304c8e65cfccd3a8e8fe6f01d57d4a10171
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-03 15:10:40 -0300

    feat: create index endpoint and api

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import routes from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+
+app.use("/contability", routes);
+
+export default app;
